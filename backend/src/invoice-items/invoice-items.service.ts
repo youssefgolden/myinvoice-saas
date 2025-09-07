821f592d9bba0a99b66baf2aecb5949bb293a09c
youssefgolden <youssefhafidi@hotmail.fr>
2025-09-07 13:01:11 +0200

    chore: add front next

--- backend/src/invoice-items/invoice-items.service.ts
+++ backend/src/invoice-items/invoice-items.service.ts
@@ -1,4 +1,3 @@
-// src/invoice-items/invoice-items.service.ts
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
