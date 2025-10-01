4e1cc673d2636a8c5dc7d471de40391086cc8e96
youssefgolden <youssefhafidi@hotmail.fr>
2025-10-01 13:38:32 +0200

    fix ci

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+console.log('[BOOT] DATABASE_URL =', process.env.DATABASE_URL);
+console.log('[BOOT] DB_SSL =', process.env.DB_SSL);
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
