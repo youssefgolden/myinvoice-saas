a2beb43e54423f0c6f29a5b2837529a34a41bf76
youssefgolden <youssefhafidi@hotmail.fr>
2025-10-01 14:05:32 +0200

    fix ci

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,6 +6,7 @@ console.log('[BOOT] DB_SSL =', process.env.DB_SSL);
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.enableCors(); // si tu appelles depuis un front externe
+  await app.listen(process.env.PORT || 3000, '0.0.0.0');
 }
 bootstrap();
